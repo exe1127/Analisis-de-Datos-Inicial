4b1b558b3437068d61acc179b33f9822c8d02ff3
Exequiel Zalazar <exequiel.zalazar@nonerds.com>
2023-08-16 12:16:56 -0300

    se completo la integracion con la api

--- function.py
+++ function.py
@@ -32,8 +32,7 @@ def normalize(request):
     # Normalizacion de la respuesta de la api
     datecols = ['dt', 'sunrise', 'sunset']
     records = []
-
-    
+    # Utiliza json_normalize para extraer y normalizar los datos
     weather = json_normalize(request['weather'])
     coord = json_normalize(request['coord'])
     main = json_normalize(request['main'])
@@ -41,18 +40,24 @@ def normalize(request):
     clouds = json_normalize(request['clouds'])
     sys = json_normalize(request['sys'])
 
+    # Crea un registro combinando todos los datos normalizados
     record = {
-            'id': request['id'], 
-            'name': request['name'],
-            'cod': request['cod'],
-            'dt': pd.to_datetime(request['dt'], unit='s'),
-            **weather.iloc[0], **coord.iloc[0], **main.iloc[0], **wind.iloc[0], **clouds.iloc[0], **sys.iloc[0]
+        'id': request['id'],
+        'name': request['name'],
+        'cod': request['cod'],
+        'dt': pd.to_datetime(request['dt'], unit='s'),
+        **weather.iloc[0], **coord.iloc[0], **main.iloc[0], **wind.iloc[0], **clouds.iloc[0], **sys.iloc[0]
     }
+
     records.append(record)
 
+    # Se genera el dataFrame correspondiente
     result_df = pd.DataFrame(records)
-    result_df = result_df.set_index('id')
 
+    # Establece el índice del DataFrame en la columna 'id' para evitar errores
+    result_df = result_df.set_index('id')
+    
+    # Convierte las columnas de fechas a objetos de fecha y hora
     result_df[datecols] = result_df[datecols].apply(pd.to_datetime)
+
     return result_df
-   
--- main.py
+++ main.py
@@ -14,30 +14,28 @@ if __name__ == '__main__':
     coordList = ["lat=31&lon=64", "lat=40&lon=-73", "lat=-31&lon=-64", "lat=25&lon=64", "lat=-34&lon=-58",
                  "lat=19&lon=-99", "lat=53&lon=6", "lat=41&lon=44", "lat=4&lon=74", "lat=35&lon=139"]
 
-    dataF = pd.DataFrame()
-    dataF2 = pd.DataFrame()
     list = []
-    cont = 0
+    date = datetime.now().strftime("%Y%m%d")
 
     # Realizamos la primera busqueda por nombre de la ciudad
     for city in cityList:
-        list.append(search(city,key))
-        
+        list.append(search(city, key))
+
     for coords in coordList:
-        # Separamos los elementos del arrregolo en longitud y latitud
+    # Separamos los elementos del arrregolo en longitud y latitud
         lat, lon = coords.split('&')
     # Extraemos los valores numéricos de latitud y longitud después de "lat=" y "lon="
         lat = float(lat.split('=')[1])
         lon = float(lon.split('=')[1])
     # Realizamos la segunda busqueda por latitud y longitud de la ciudad
-        cont = cont+1
         list.append(search2(lat, lon, key))
 
-    normalized_data = [normalize(entry) for entry in data]
+    # Se genera la normalizacion de la lista obtenida en las busquedas
+    normalized_data = [normalize(entry) for entry in list]
 
+    # Se consolida en un dataFrame
     consolidated_df = pd.concat(normalized_data)
 
-    date = datetime.now().strftime("%Y%m%d")
-
-    consolidated_df.to_csv(f"data_analytics/openweather/Archivo_{date}.csv", index=False)
-    
+    # Se guarda en un archivo .csv el dataFrame generado y finaliza el proceso
+    consolidated_df.to_csv(
+        f"data_analytics/openweather/Archivo_{date}.csv", index=False)
